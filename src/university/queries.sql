60dbfb905464d370b7208495dbcf6138ac3aef5f
Paula <paula.uxengineer@gmail.com>
2024-04-11 18:31:25 +0200

    up to 46 queries

--- src/university/queries.sql
+++ src/university/queries.sql
@@ -63,3 +63,15 @@ SELECT d.nom_departament, COUNT(p.id_professor) AS num_professors FROM departame
 
 --5 Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun. Tingues en compte que poden existir graus que no tenen assignatures associades. Aquests graus també han d'aparèixer en el llistat. El resultat haurà d'estar ordenat de major a menor pel nombre d'assignatures.
 SELECT g.nom_grau, COUNT(a.id_assignatura) AS num_assignatures FROM grau g LEFT JOIN assignatura a ON g.id_grau = a.id_grau GROUP BY g.nom_grau ORDER BY num_assignatures DESC;
+
+--6 Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun, dels graus que tinguin més de 40 assignatures associades.
+
+--7 Retorna un llistat que mostri el nom dels graus i la suma del nombre total de crèdits que hi ha per a cada tipus d'assignatura. El resultat ha de tenir tres columnes: nom del grau, tipus d'assignatura i la suma dels crèdits de totes les assignatures que hi ha d'aquest tipus.
+
+--8 Retorna un llistat que mostri quants alumnes s'han matriculat d'alguna assignatura en cadascun dels cursos escolars. El resultat haurà de mostrar dues columnes, una columna amb l'any d'inici del curs escolar i una altra amb el nombre d'alumnes matriculats.
+
+--9 Retorna un llistat amb el nombre d'assignatures que imparteix cada professor/a. El llistat ha de tenir en compte aquells professors/es que no imparteixen cap assignatura. El resultat mostrarà cinc columnes: id, nom, primer cognom, segon cognom i nombre d'assignatures. El resultat estarà ordenat de major a menor pel nombre d'assignatures.
+
+--10 Retorna totes les dades de l'alumne/a més jove.
+
+--11 Retorna un llistat amb els professors/es que tenen un departament associat i que no imparteixen cap assignatura.
