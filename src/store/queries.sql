c3aeca9059ab8bebee8d924c89a42b9da161d3dd
Paula <157831233+paula-uxengineer@users.noreply.github.com>
2024-05-16 19:21:13 +0200

    Update queries.sql

--- src/store/queries.sql
+++ src/store/queries.sql
@@ -1,11 +1,11 @@
 --1 Llista el nom de tots els productes que hi ha en la taula producto.
-SELECT nombre FROM productos;
+SELECT nombre FROM producto;
 
 --2 Llista els noms i els preus de tots els productes de la taula producto.
 SELECT nombre, precio FROM producto;
 
 --3 Llista totes les columnes de la taula producto.
-SELECT * FROM productos;
+SELECT * FROM producto;
 
 --4 Llista el nom dels productes, el preu en euros i el preu en dòlars estatunidencs (USD).
 
